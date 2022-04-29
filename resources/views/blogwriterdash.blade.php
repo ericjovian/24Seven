488be7c264d8985b2d16cef977f0a2ddc6b96fe9
Eric Jovian <43366082+ericjovian@users.noreply.github.com>
2022-04-29 23:36:31 +0700

    new login page

--- resources/views/blogwriterdash.blade.php
+++ resources/views/blogwriterdash.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard for Road Professional') }}
+            {{ __('Dashboard for Roadside Professional') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in as a Professional!
+                    Welcome to Roadside Professional's Dashboard.<br>
+                    Jobs Completed :<em style="color:dodgerblue">14</em><br>
+                    Current Earnings :<em style="color:green">$2,143</em>
                 </div>
             </div>
         </div>
--- resources/views/postcreate.blade.php
+++ resources/views/postcreate.blade.php
@@ -8,11 +8,17 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Joko Widodo <br>
-                    Honda Jazz - B 1234 ZZ
-                    <br>
-                    Ban Kempes
+                <div class="d-flex p-6 flex-row">
+                    <div>
+                        Joko Widodo <br>
+                        Honda Jazz - B 1234 ZZ
+                        <br>
+                        Ban Kempes
+                    </div>
+                    <div>
+                        <x-tick class="block h-20 w-auto fill-current text-gray-600" />
+                        <x-cross class="block h-20 w-auto fill-current text-gray-600" />
+                    </div>
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
                     Jusuf Kalla <br>
