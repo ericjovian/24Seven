4683f514d3ce15b360091a55270cdde34521d509
Winson Wardana <winson91100@gmail.com>
2022-05-20 23:14:06 +1000

    update membership

--- resources/views/membership.blade.php
+++ resources/views/membership.blade.php
@@ -10,19 +10,20 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5" style="text-align:center;">
               <h1 style="padding-bottom:1vw;font-size:2vw;font-family:montserrat-bold">Membership Benefits</h1>
               <hr>
-              <div class="row m-0" style="padding-top:2vw">
+              <div class="row m-0" style="padding-top:2vw;padding-bottom:2vw">
 
                 <div class="col-md-4">
-                  <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="/assets/customer-page/membership1.jpg" alt="Card image cap">
+                  <div class="card" style="width: 18rem;border:none">
+                    <img class="card-img-top" src="/assets/customer-page/membership1.jpg" style="" class="img-fluid" alt="Card image cap">
                     <div class="card-body">
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                   </div>
                 </div>
+                
 
                 <div class="col-md-4">
-                  <div class="card" style="width: 18rem;">
+                  <div class="card" style="width: 18rem;border:none">
                     <img class="card-img-top" src="/assets/customer-page/membership.jpg" class="img-fluid" alt="Card image cap">
                     <div class="card-body">
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -31,15 +32,19 @@
                 </div>
 
                 <div class="col-md-4">
-                  <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="/assets/customer-page/membership1.jpg" alt="Card image cap">
-                    <div class="card-body">
+                  <div class="card" style="width: 18rem;border:none">
+                    <img class="card-img-top" src="/assets/customer-page/membership1.jpg" class="img-fluid" alt="Card image cap">
+                    <div class="card-body" s>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
                   </div>
                 </div>
 
-              </div>              
-            </iv>           
+              </div>
+              <hr>
+                            
+            </div> 
+                    
         </div>
     </div>
 </x-app-layout>
